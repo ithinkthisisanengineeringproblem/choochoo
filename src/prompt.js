e0cc9193563356cc14704b4fd45c94f533d4b6da
serverhiccups <hiccup@hiccup01.com>
2020-01-11 17:48:09 +1300

    bler

--- src/prompt.js
+++ src/prompt.js
@@ -1,7 +1,18 @@
+import TextProtoReader from "https://deno.land/std/textproto/mod.ts";
+import BufReader from "https://deno.land/std/io/mod.ts";
+
 export default async function getInput(cin, cout, prompt) {
 	const encoder = new TextEncoder();
-	await Deno.write(cout.rid, encoder.encode(prompt));
-	let buf = new Uint8Array(100);
-	Deno.read(cin.rid, buf);
-	return new TextDecoder().decode(buf);
+	cout.write(encoder.encode(prompt));
+	return getLine(cin);
+}
+
+async function* getLine(cin) {
+	let tp = new TextProtoReader(new bf(cin));
+	let buff;
+	while(buff != Deno.EOF) {
+		buff = await tp.readLine();
+		if(Deno.EOF === buff) break;
+		yield buff;
+	}
 }
